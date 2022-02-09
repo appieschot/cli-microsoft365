a969ad36f9be7bcfafdc2c0480bd8de84c322f65
Albert-Jan Schot <appie@digiwijs.nl>
2022-02-09 15:56:07 +0100

    Changed to graph url. Solves #2923

--- src/m365/flow/commands/flow-export.ts
+++ src/m365/flow/commands/flow-export.ts
@@ -87,7 +87,7 @@ class FlowExportCommand extends AzmgmtCommand {
         }
 
         const requestOptions: any = {
-          url: `${this.resource}providers/${formatArgument === 'json' ?
+          url: `https://api.bap.microsoft.com/providers/${formatArgument === 'json' ?
             `Microsoft.ProcessSimple/environments/${encodeURIComponent(args.options.environment)}/flows/${encodeURIComponent(args.options.id)}?api-version=2016-11-01`
             : `Microsoft.BusinessAppPlatform/environments/${encodeURIComponent(args.options.environment)}/exportPackage?api-version=2016-11-01`}`,
           headers: {
